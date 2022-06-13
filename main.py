261f7d8bab65024877b911a1eb633648d0940e82
Kris Reid <krisreid_06@hotmail.com>
2022-06-13 17:37:49 +0100

    moving SQLLite to Postgres part 2

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ random = Random()
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///cafes.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
