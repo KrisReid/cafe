7a0dc2e8558e0d8f84333b726657b227d6676edb
Kris Reid <krisreid_06@hotmail.com>
2022-06-13 17:34:04 +0100

    moving SQLLite to Postgres

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from random import Random
+import os
 
 random = Random()
-
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
